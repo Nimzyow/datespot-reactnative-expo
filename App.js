3e3086da9472ca90fb6ee2fee9961ac65f094ee1
Nimzyow <n_soufiani@hotmail.com>
2020-06-11 20:23:32 +0100

    native base installed

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Text } from "native-base";
 
 export default function App() {
   return (
