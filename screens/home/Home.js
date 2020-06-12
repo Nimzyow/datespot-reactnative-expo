3dd2f1947f1fedf326bf8e0f0a06f3f4321d72c1
Nimzyow <n_soufiani@hotmail.com>
2020-06-12 16:45:16 +0100

    fixed test not testing native-base components

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View } from "react-native";
+import { Button, Text } from "native-base";
 
 export const Home = () => {
   return (
     <View accessibilityLabel="homeContainer">
-      <Button title="Sign in" accessibilityLabel="signInButton" />
+      <Text>LAURA</Text>
+      <Button title="Sign in" accessibilityLabel="signInButton">
+        <Text>Hello</Text>
+      </Button>
       <Button title="Register" accessibilityLabel="registerButton" />
     </View>
   );
