ce2ad1977d21b650f93870e6adc07d79cbf5a557
Nimzyow <n_soufiani@hotmail.com>
2020-06-15 22:01:24 +0100

    Created Spots screen and added it to screen stack

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 import { Home } from "./screens/home/Home";
+import { Spots } from "./screens/spots/Spots";
 import Authentication from "./screens/authentication/Authentication";
 
 const App = () => {
@@ -22,6 +23,11 @@ const App = () => {
           <Stack.Screen
             name="authentication"
             component={Authentication}
+            options={{ title: "DateSpot" }}
+          />
+          <Stack.Screen
+            name="spots"
+            component={Spots}
             options={{ title: "DateSpot" }}
           />
         </Stack.Navigator>
