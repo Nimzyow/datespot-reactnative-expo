5426ffc41558a600ef8ff8f461fbf268694af64c
Nimzyow <n_soufiani@hotmail.com>
2020-06-15 22:04:01 +0100

    Created spots page

---
+++ screens/spots/Spots.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Spots = () => {
+  return (
+    <View>
+      <Text>we are in spots</Text>
+    </View>
+  );
+};
